1e2b721493fe7e192bc2956f779ef65c52064e58
Mariia Konovalchuk <71904680+MariiaKon@users.noreply.github.com>
2022-05-04 01:46:31 +0300

    removed form autocomplete

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -40,6 +40,7 @@ export class ContactForm extends Component {
             pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
             title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
             required
+            autoComplete="off"
             value={this.state.name}
             onChange={this.inputChangeHandler}
           />
@@ -52,6 +53,7 @@ export class ContactForm extends Component {
             pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
             title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
             required
+            autoComplete="off"
             value={this.state.number}
             onChange={this.inputChangeHandler}
           />
