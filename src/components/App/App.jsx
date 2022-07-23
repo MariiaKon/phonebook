927a94a81cf4f00f411946639ba321288c0ade69
Mariia Konovalchuk <71904680+MariiaKon@users.noreply.github.com>
2022-07-23 03:09:49 +0300

    fixed Routes, added WelcomePage

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,49 +1,85 @@
-import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import { lazy, Suspense } from 'react';
+import { useSelector } from 'react-redux';
 import { Loader } from 'components/Loader/Loader';
+import img from 'images/phonebook-welcome.png';
+const Usermenu = lazy(() => import('components/Usermenu/Usermenu'));
 const RegisterForm = lazy(() => import('views/Register/RegisterView'));
 const LoginForm = lazy(() => import('views/Login/LogInView'));
 const ContactsView = lazy(() => import('views/Contacts/ContactsView'));
 const Modal = lazy(() => import('views/EditContactModal/EditContactView'));
 
 export function App() {
-  return (
-    <BrowserRouter basename="/goit-react-hw-08-phonebook/">
-      <Routes>
-        <Route path="/" element={<Navigate to="register" />} />
+  const isLoggedIn = useSelector(state => state.user.isLoggedIn);
 
+  return (
+    <BrowserRouter basename="/goit-react-hw-08-phonebook">
+      <Routes>
         <Route
-          path="register"
+          path="/"
           element={
             <Suspense fallback={<Loader />}>
-              <RegisterForm />
+              <Usermenu />
             </Suspense>
           }
-        />
-        <Route
-          path="login"
-          element={
-            <Suspense fallback={<Loader />}>
-              <LoginForm />
-            </Suspense>
-          }
-        />
-        <Route
-          path="contacts"
-          element={
-            <Suspense fallback={<Loader />}>
-              <ContactsView />
-            </Suspense>
-          }
-        />
-        <Route
-          path="edit/:contactId"
-          element={
-            <Suspense fallback={<Loader />}>
-              <Modal />
-            </Suspense>
-          }
-        />
+        >
+          <Route
+            index
+            element={
+              <Suspense fallback={<Loader />}>
+                <main
+                  style={{
+                    height: '90vh',
+                    backgroundImage: `linear-gradient(90deg, #38170a83, #38170a83), url(${img})`,
+                    backgroundRepeat: 'no-repeat',
+                    backgroundPosition: 'center',
+                    backgroundSize: 'cover',
+                  }}
+                />
+              </Suspense>
+            }
+          />
+          <Route
+            path="register"
+            element={
+              <Suspense fallback={<Loader />}>
+                <RegisterForm />
+              </Suspense>
+            }
+          />
+          <Route
+            path="login"
+            element={
+              <Suspense fallback={<Loader />}>
+                <LoginForm />
+              </Suspense>
+            }
+          />
+          <Route
+            path="contacts"
+            element={
+              isLoggedIn ? (
+                <Suspense fallback={<Loader />}>
+                  <ContactsView />
+                </Suspense>
+              ) : (
+                <Navigate to="/register" />
+              )
+            }
+          />
+          <Route
+            path="edit/:contactId"
+            element={
+              isLoggedIn ? (
+                <Suspense fallback={<Loader />}>
+                  <Modal />
+                </Suspense>
+              ) : (
+                <Navigate to="/register" />
+              )
+            }
+          />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
