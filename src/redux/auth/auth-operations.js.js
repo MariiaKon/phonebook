522d793c73a1d6d43619a8ad2305b9b2208f3f29
Mariia Konovalchuk <71904680+MariiaKon@users.noreply.github.com>
2022-09-05 22:18:38 +0300

    renamed files, added createAsyncThunk

---
+++ src/redux/auth/auth-operations.js.js
@@ -0,0 +1,58 @@
+import axios from 'axios';
+import { createAsyncThunk } from '@reduxjs/toolkit';
+
+axios.defaults.baseURL = 'https://connections-api.herokuapp.com';
+
+const token = {
+  set(token) {
+    axios.defaults.headers.common.Authorization = `Bearer ${token}`;
+  },
+  unset() {
+    axios.defaults.headers.common.Authorization = '';
+  },
+};
+
+const register = createAsyncThunk('auth/register', async userData => {
+  try {
+    const { data } = await axios.post('/users/signup', userData);
+    token.set(data.token);
+    return data;
+  } catch (error) {}
+});
+
+const login = createAsyncThunk('auth/login', async userData => {
+  try {
+    const { data } = await axios.post('/users/login', userData);
+    token.set(data.token);
+    return data;
+  } catch (error) {}
+});
+
+const logout = createAsyncThunk('auth/logout', async () => {
+  try {
+    await axios.post('/users/logout');
+    token.unset();
+  } catch (error) {}
+});
+
+const fetchCurrentUser = createAsyncThunk(
+  'auth/fetchCurrentUser',
+  async (_, thunkAPI) => {
+    const state = thunkAPI.getState();
+    const persistedToken = state.auth.token;
+
+    if (persistedToken === null) {
+      return thunkAPI.rejectWithValue();
+    }
+
+    token.set(persistedToken);
+    try {
+      const { data } = await axios.get('/users/current');
+      return data;
+    } catch (error) {}
+  }
+);
+
+const authOperations = { register, login, logout, fetchCurrentUser };
+
+export default authOperations;
---
+++ src/redux/auth/authSlice.js
@@ -0,0 +1,32 @@
+import { createSlice } from '@reduxjs/toolkit';
+import authOperations from './auth-operations.js';
+
+const initialState = {
+  user: { name: '', email: '', password: '' },
+  token: null,
+  isLoggedIn: false,
+};
+
+export const authSlice = createSlice({
+  name: 'auth',
+  initialState,
+  extraReducers: {
+    [authOperations.register.fulfilled](state, { payload }) {
+      state.user = payload.user;
+      state.token = payload.token;
+      state.isLoggedIn = true;
+    },
+    [authOperations.login.fulfilled](state, { payload }) {
+      state.user = payload.user;
+      state.token = payload.token;
+      state.isLoggedIn = true;
+    },
+    [authOperations.logout.fulfilled](state) {
+      return (state = initialState);
+    },
+    [authOperations.fetchCurrentUser.fulfilled](state, { payload }) {
+      state.user = payload;
+      state.isLoggedIn = true;
+    },
+  },
+});
